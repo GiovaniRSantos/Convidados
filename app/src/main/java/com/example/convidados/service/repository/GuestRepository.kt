0b5a753771af8f80e651a0b2d1f67aa6d304344b
Giovani Rodrigues <giovani.srodrigues@hotmail.com>
2022-04-13 20:05:13 -0300

    funcao salvar convidado

--- app/src/main/java/com/example/convidados/service/repository/GuestRepository.kt
+++ app/src/main/java/com/example/convidados/service/repository/GuestRepository.kt
@@ -1,8 +1,43 @@
 package com.example.convidados.service.repository
 
+import android.content.ContentValues
+import android.content.Context
+import android.provider.ContactsContract
+import com.example.convidados.constants.DataBaseConstants
 import com.example.convidados.service.model.GuestModel
+import java.lang.Exception
 
-class GuestRepository {
+class GuestRepository private constructor(context: Context) {
+
+    private var mGuestDataBaseHelper: GuestDataBaseHelper = GuestDataBaseHelper(context)
+
+    companion object {
+        private lateinit var repository: GuestRepository
+
+        fun getInstance(context: Context): GuestRepository {
+            if (!::repository.isInitialized) {
+                repository = GuestRepository(context)
+            }
+            return GuestRepository(context)
+        }
+    }
+
+    fun save(guest: GuestModel): Boolean {
+        return try {
+            val db = mGuestDataBaseHelper.writableDatabase
+
+            val contentValues = ContentValues()
+            contentValues.put(DataBaseConstants.GUEST.COLUMNS.NAME, guest.name)
+            contentValues.put(DataBaseConstants.GUEST.COLUMNS.PRESENCE, guest.presence)
+            db.insert(DataBaseConstants.GUEST.TABLE_NAME, null, contentValues)
+            true
+        } catch (e: Exception) {
+            false
+        }
+
+
+    }
+
     fun getAll(): List<GuestModel> {
         val list: MutableList<GuestModel> = ArrayList()
         return list
@@ -16,10 +51,6 @@ class GuestRepository {
     fun getAbsent(): List<GuestModel> {
         val list: MutableList<GuestModel> = ArrayList()
         return list
-    }
-
-    fun save(guest: GuestModel) {
-
     }
 
     fun update(guest: GuestModel) {
--- app/src/main/java/com/example/convidados/viewmodel/GuestFormViewModel.kt
+++ app/src/main/java/com/example/convidados/viewmodel/GuestFormViewModel.kt
@@ -1,13 +1,17 @@
 package com.example.convidados.viewmodel
 
+import android.annotation.SuppressLint
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import com.example.convidados.service.model.GuestModel
 import com.example.convidados.service.repository.GuestRepository
 
-class GuestFormViewModel : ViewModel() {
-    private val mGuestRepository: GuestRepository = GuestRepository()
+class GuestFormViewModel(application: Application) : AndroidViewModel(application) {
+    @SuppressLint("StaticFieldLeak")
+    private val mContext = application.applicationContext
+    private val mGuestRepository: GuestRepository = GuestRepository.getInstance(mContext)
 
     private var mSaveGuest = MutableLiveData<Boolean>()
     val saveGuest: LiveData<Boolean> = mSaveGuest
